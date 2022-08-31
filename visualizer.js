a075c639d053e87f0507f38d361276ea61ae8b56
Vince P <32515747+Peperito@users.noreply.github.com>
2022-08-31 17:57:24 +0200

    "Visualizer 1.0"

--- visualizer.js
+++ visualizer.js
@@ -6,13 +6,31 @@ class Visualizer{
         const width = ctx.canvas.width-margin*2;
         const height = ctx.canvas.height-margin*2;
 
-        Visualizer.drawLevel(ctx, network.levels[0], left, top, width, height);
+        const levelHeight = height/network.levels.length;
+
+        for(let i=network.levels.length-1; i>=0; i--){
+            const levelTop = top+
+                lerp(
+                    height-levelHeight,
+                    0,
+                    network.levels.length==1
+                        ?0.5
+                        :i/(network.levels.length-1)
+                )
+            ctx.setLineDash([7,3]);
+            Visualizer.drawLevel(ctx, network.levels[i],
+                left, levelTop,
+                width, levelHeight,
+                i==network.levels.length-1
+                    ?["↑","←","→","↓"]
+                    :[]);
+        };
     }
 
-    static drawLevel(ctx, level, left, top, width, height){
+    static drawLevel(ctx, level, left, top, width, height, outputLabels){
         const right = left + width;
         const bottom = top + height;
-        const {inputs, outputs, weights} = level;
+        const {inputs, outputs, weights, biases} = level;
 
         const nodeRadius = 18;
 
@@ -28,12 +46,7 @@ class Visualizer{
                     top
                 );
                 ctx.lineWidth=2;
-                const value = weights[i][j];
-                const alpha = Math.abs(value);
-                const R=value<0?0:255;
-                const G=R;
-                const B = value>0?0:255;
-                ctx.strokeStyle="rgba("+R+","+G+","+B+","+alpha+")";
+                ctx.strokeStyle=getRGBA(weights[i][j]);
                 ctx.stroke();
             }
         }
@@ -44,7 +57,11 @@ class Visualizer{
             );
             ctx.beginPath();
             ctx.arc(x, bottom, nodeRadius, 0, Math.PI*2);
-            ctx.fillStyle="white";
+            ctx.fillStyle="black";
+            ctx.fill();
+            ctx.beginPath();
+            ctx.arc(x, bottom, nodeRadius*0.6, 0, Math.PI*2);
+            ctx.fillStyle=getRGBA(inputs[i]);
             ctx.fill();
         }
 
@@ -54,8 +71,32 @@ class Visualizer{
             );
             ctx.beginPath();
             ctx.arc(x, top, nodeRadius, 0, Math.PI*2);
-            ctx.fillStyle="white";
+            ctx.fillStyle="black";
             ctx.fill();
+            ctx.beginPath();
+            ctx.arc(x, top, nodeRadius*0.6, 0, Math.PI*2);
+            ctx.fillStyle=getRGBA(outputs[i]);
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.lineWidth=2;
+            ctx.arc(x,top,nodeRadius*0.8,0,Math.PI*2);
+            ctx.strokeStyle=getRGBA(biases[i]);
+            ctx.setLineDash([3,3]);
+            ctx.stroke();
+            ctx.setLineDash([]);
+
+            if(outputLabels[i]){
+                ctx.beginPath();
+                ctx.textAlign="center";
+                ctx.textBaseline="middle";
+                ctx.fillStyle="black";
+                ctx.strokeStyle="white";
+                ctx.font=(nodeRadius*1.5)+"px Arial";
+                ctx.fillText(outputLabels[i],x,top);
+                ctx.lineWidth=0.5;
+                ctx.strokeText(outputLabels[i],x,top);
+            }
         }
     }
 
@@ -64,4 +105,6 @@ class Visualizer{
             left, right, nodes.length==1?0.5:index/(nodes.length -1)
         );
     }
+
+
 }
